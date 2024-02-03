44baead065c92f4be23dd3e9b53ee317c3f59d2e
jordantdale98 <jordantdale98@gmail.com>
2024-02-03 06:53:04 -0500

    need to fix login and auth user functions

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,23 +6,19 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  const username = req.params.username;
-  const password = req.params.password;
-
-  if (username && password){
-
-    if(!doesExist(username)){
-        users.push({"username": username, "password": password});
-
-        return res.status(200).json({message: "User successfully registered!"});
-    }
-    else {
-        return res.status(404).json({message: "User already exists"});
-    }
-}
-    return res.status(404).json({message: "Unable to register user, please try again."});
-});
+    const username = req.query.username;
+    const password = req.query.password;
+  
+    if (username && password) {
+      if (!isValid(username)) { 
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "User successfully registred. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User already exists!"});    
+      }
+    } 
+    return res.status(404).json({message: "Unable to register user."});
+  });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
@@ -55,7 +51,6 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   const isbn = req.params.isbn;
-  //let requested_review = Object.values(books).filter((book) => book.isbn === isbn);
 
   res.send(books[isbn].reviews);
 
